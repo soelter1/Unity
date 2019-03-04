254efe6351bf5113768eceeab7eef93b7ee6ea2b
senorrobotico <darkprinceofpersia@hotmail.com>
2019-03-04 17:58:12 +0100

    Update

---
+++ GBTBSP/Assets/Scripts/Cursor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    public MoveAbleObject selectedTarget;
+    public MoveAbleObject onObject;
+    public SelectedUI selectedUI;
+    public GridBehaviourScript grid;
+
+    public int gridsizeX;
+    public int gridsizeZ;
+
+    bool targetSelected = false;
+
+    public bool onTarget = false;
+    public bool blocked = false;
+
+    private void OnGUI()
+    {
+        if(Input.GetKeyDown("w") && !(transform.position.z == (gridsizeZ*10)))
+        {
+            onTarget = false;
+            blocked = false;
+            transform.position = (transform.position+(new Vector3(0, 0, 2.5f)));
+        }
+        if (Input.GetKeyDown("s") && !(transform.position.z == 0f))
+        {
+            onTarget = false;
+            blocked = false;
+            transform.position = (transform.position + (new Vector3(0, 0, -2.5f)));
+        }
+        if (Input.GetKeyDown("a") && !(transform.position.x == 0f))
+        {
+            onTarget = false;
+            blocked = false;
+            transform.position = (transform.position + (new Vector3(-2.5f,0,0)));
+        }
+        if (Input.GetKeyDown("d") && !(transform.position.x == (gridsizeX*10f)))
+        {
+            onTarget = false;
+            blocked = false;
+            transform.position = (transform.position + (new Vector3(2.5f, 0, 0)));
+        }
+        if (Input.GetKeyDown("space") && targetSelected && !(blocked || onTarget))
+        {
+            selectedTarget.transform.position = new Vector3(transform.position.x, selectedTarget.transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown("f"))
+        {
+            if (onTarget)
+            {
+                selectedTarget = onObject;
+                targetSelected = true;
+                selectedUI.UpdateName(selectedTarget.unitName);
+            }
+            else
+            {
+                selectedTarget = null;
+                targetSelected = false;
+                selectedUI.UpdateName("None");
+            }
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            grid.showMovementRange(selectedTarget);
+        }
+    }
+
+}
---
+++ GBTBSP/Assets/Scripts/GridBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehaviourScript : MonoBehaviour
+{
+
+   public void showMovementRange(MoveAbleObject obj)
+    {
+
+        Debug.Log(obj.transform.position);
+
+        FloorBehaviourScript[] floors = this.GetComponents<FloorBehaviourScript>();
+
+        foreach(FloorBehaviourScript i in floors)
+        {
+            if(i.transform.position == new Vector3(obj.transform.position.x, 0, obj.transform.position.z))
+            {
+                i.transform.position = new Vector3(0, 0, 110);
+            }
+        }
+
+        //int range = obj.movementRange;
+
+        //Vector3 position= obj.transform.position;
+    }
+}
