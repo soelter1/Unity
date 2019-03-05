2bec3c73d5be9adf1a156700707576363cf56f12
1nfinite-Zer0 <48217991+1nfinite-Zer0@users.noreply.github.com>
2019-03-05 12:17:40 +0100

    moverange 0.05

--- GBTBSP/Assets/Scripts/GridBehaviourScript.cs
+++ GBTBSP/Assets/Scripts/GridBehaviourScript.cs
@@ -2,26 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+
 public class GridBehaviourScript : MonoBehaviour
 {
 
-   public void showMovementRange(MoveAbleObject obj)
+    public FloorBehaviourScript[] floors;
+
+    Renderer rend;
+
+    Shader Ground1;
+    Shader Ground2;
+    Shader Range;
+
+    public void showMovementRange(MoveAbleObject obj)
     {
 
         Debug.Log(obj.transform.position);
 
-        FloorBehaviourScript[] floors = this.GetComponents<FloorBehaviourScript>();
+        floors = GetComponentsInChildren<FloorBehaviourScript>();
 
-        foreach(FloorBehaviourScript i in floors)
+        Debug.Log(floors.Length);
+
+        Vector3 objPos = obj.transform.position;
+
+        int movementRange = obj.movementRange;
+
+        int primeRange = movementRange * 2 + 1;
+
+        Debug.Log("movementRange: " + movementRange + "primeRange: " + primeRange);
+
+        Vector3[] rangeArray = new Vector3[primeRange * primeRange];
+
+        Debug.Log("rangeArray Length: " + rangeArray.Length);
+
+        Vector3[] reachable = makeRangeArray(rangeArray, objPos, movementRange);
+
+        foreach (FloorBehaviourScript i in floors)
         {
-            if(i.transform.position == new Vector3(obj.transform.position.x, 0, obj.transform.position.z))
+            bool a = Array.Exists<Vector3>(reachable, Vector3 => Vector3 == i.transform.position); //new Vector3(obj.transform.position.x, 0, obj.transform.position.z)
+
+            if (a)
             {
-                i.transform.position = new Vector3(0, 0, 110);
+                Debug.Log(i + "is reachable");
+                rend = i.GetComponent<MeshRenderer>();
+
+                rend.material.shader = Range;
             }
         }
+    }
 
-        //int range = obj.movementRange;
+    Vector3[] makeRangeArray(Vector3[] rangeArray, Vector3 objPos, int movementRange)
+    {
+        //rangeArray[0] = objPos;
 
-        //Vector3 position= obj.transform.position;
+        rangeArray[0] = new Vector3(objPos.x, 0, objPos.z + (movementRange * 10));
+
+        return rangeArray;
     }
 }
