c2917ce617ab24885d24ca5a7e2fcd0cc78f5fdf
1nfinite-Zer0 <48217991+1nfinite-Zer0@users.noreply.github.com>
2019-03-07 15:45:12 +0100

    luk an explosion

---
+++ GBTBSP/Assets/Scripts/DestroyBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBehaviourScript : MonoBehaviour
+{
+
+    public float radius = 5.0f;
+    public float force = 10.0f;
+
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            Collider[] colliders = Physics.OverlapSphere(hit.point, radius);
+
+            foreach (Collider c in colliders)
+            {
+                c.attachedRigidbody.AddExplosionForce(force, hit.point, radius, 0.5f, ForceMode.Impulse);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
