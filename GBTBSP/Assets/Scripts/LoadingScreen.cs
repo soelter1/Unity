bfebdc84d00d133d161c8f47d8aa4864fcfcfbf0
soelter1 <b.soelter@live.de>
2019-03-13 20:45:28 +0100

    loadingScreen

---
+++ GBTBSP/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+
+    public Image bar;
+    void Update()
+    {
+       if(bar.fillAmount > 0.99 )
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
